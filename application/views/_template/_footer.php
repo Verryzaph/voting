55dffc3ab7e36fbebed99e8eac655fe3cdec3217
Verryzaph <49016217+Verryzaph@users.noreply.github.com>
2025-06-25 16:23:04 +0700

    Add files via upload

--- application/views/_template/_footer.php
+++ application/views/_template/_footer.php
@@ -1,38 +1,28 @@
-    </section>
-    <!-- /.content -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong> &copy; <?=Date('Y')?></strong> | Developed By Muhammad Zulfi Izzulhaq
     </div>
-    <strong> &copy; <?=Date('Y')?></strong> | Developed By Muhammad Zulfi Izzulhaq
+    <!-- /.container -->
   </footer>
-
 </div>
 <!-- ./wrapper -->
 
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
 <!-- Bootstrap 3.3.7 -->
-<script src="<?=base_url('./../assets/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js');?>"></script>
-<!-- jQuery Knob Chart -->
-<script src="<?=base_url('./../assets/adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js');?>"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="<?=base_url('./../assets/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js');?>"></script>
-<!-- DataTables -->
-<script src="<?=base_url('./../assets/adminlte/bower_components/datatables.net/js/jquery.dataTables.min.js');?>"></script>
-<script src="<?=base_url('./../assets/adminlte/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js');?>"></script>
+<script src="<?=base_url('assets/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js');?>"></script>
 <!-- FastClick -->
-<script src="<?=base_url('./../assets/adminlte/bower_components/fastclick/lib/fastclick.js');?>"></script>
+<script src="<?=base_url('assets/adminlte/bower_components/fastclick/lib/fastclick.js');?>"></script>
 <!-- AdminLTE App -->
-<script src="<?=base_url('./../assets/adminlte/dist/js/adminlte.min.js');?>"></script>
-<script>
-  $(function(){
-    $('.dt').DataTable()
-  })
-</script>
+<script src="<?=base_url('assets/adminlte/dist/js/adminlte.min.js');?>"></script>
+<?=($this->session->flashdata('login_gagal')) ? '<script>Swal.fire("Failed", "Username/Password Incorrect !", "error")</script>' : '' ?>
+<?=($this->session->flashdata('novoting')) ? '<script>Swal.fire("Failed", "No voting !", "error")</script>' : '' ?>
 </body>
 </html>
--- application/views/_template/_header.php
+++ application/views/_template/_header.php
@@ -3,31 +3,21 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?=$title;?> | Administrator</title>
+  <title><?=$title;?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css');?>">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/bower_components/font-awesome/css/font-awesome.min.css');?>">
-  <!-- DataTables -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css');?>">
-  <!-- Select2 -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/bower_components/select2/dist/css/select2.min.css')?>">
-  <!-- bootstrap datetime picker -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/css/bootstrap-datetimepicker.min.css')?>">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css');?>">
-  <!-- Pace style -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/plugins/pace/pace.min.css');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/adminlte/bower_components/font-awesome/css/font-awesome.min.css');?>">
   <!-- Sweetalert -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/css/sweetalert2.min.js');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/css/sweetalert2.min.css')?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/dist/css/AdminLTE.min.css');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/adminlte/dist/css/AdminLTE.min.css');?>">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="<?=base_url('./../assets/adminlte/dist/css/skins/_all-skins.min.css');?>">
-  <link rel="stylesheet" href="<?=base_url('./../assets/css/admin.css');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/adminlte/dist/css/skins/_all-skins.min.css');?>">
+  <link rel="stylesheet" href="<?=base_url('assets/css/admin.css');?>">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,48 +28,73 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
   <!-- jQuery 3 -->
-  <script src="<?=base_url('./../assets/adminlte/bower_components/jquery/dist/jquery.min.js');?>"></script>
-  <!-- jQuery UI 1.11.4 -->
-  <script src="<?=base_url('./../assets/adminlte/bower_components/jquery-ui/jquery-ui.min.js');?>"></script>
-  <!-- PACE -->
-  <script src="<?=base_url('./../assets/adminlte/bower_components/PACE/pace.min.js');?>"></script>
+  <script src="<?=base_url('assets/adminlte/bower_components/jquery/dist/jquery.min.js');?>"></script>
   <!-- Sweetalert -->
-  <script src="<?=base_url('./../assets/js/sweetalert2.all.min.js');?>"></script>
-  <!-- Select2 -->
-  <script src="<?=base_url('./../assets/adminlte/bower_components/select2/dist/js/select2.full.min.js')?>"></script>
-  <!-- bootstrap datetime picker -->
-  <script src="<?=base_url('./../assets/adminlte/bower_components/moment/min/moment.min.js');?>"></script>
-  <script src="<?=base_url('./../assets/js/bootstrap-datetimepicker.min.js')?>"></script>
-  <script>
-    var base_url = '<?=base_url()?>';
-  </script>
-</head>
-<body class="hold-transition fixed skin-green sidebar-mini">
+  <script src="<?=base_url('assets/js/sweetalert2.all.min.js')?>"></script>
+  </head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-green layout-top-nav">
 <div class="wrapper">
 
   <header class="main-header">
-    <?php require_once('_topmenu.php');?>
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="" class="navbar-brand"><b>Voting-</b>System</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <!-- <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div> -->
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+          <ul class="nav navbar-nav">
+            <!-- User Account Menu -->
+            <li class="dropdown">
+              <!-- Menu Toggle Button -->
+              <a><span class="hidden-xs"><?=($this->session->login) ? $this->session->nama : 'Welcome' ?></span></a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
   </header>
-  <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
-    <?php require_once('_sidebar.php');?>
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
+  <!-- Full Width Column -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        <?=$title;?>
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="<?=base_url();?>"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active"><?=$title;?></li>
-      </ol>
-    </section>
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          <?=$title;?>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active"><?=$title;?></li>
+        </ol>
+      </section>
 
-    <!-- Main content -->
-    <section class="content">
+      <!-- Main content -->
+      <section class="content">
